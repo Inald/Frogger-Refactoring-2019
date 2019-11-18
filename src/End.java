b1f3e663fd0f922bd7f634c3515dcf537c646bd0
psyig <psyig@exmail.nottingham.ac.uk>
2019-11-18 22:09:49 +0000

    Initial commit

---
+++ src/End.java
@@ -0,0 +1,26 @@
+import javafx.scene.image.Image;
+
+public class End extends Actor{
+	boolean activated = false;
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method st
+	}
+	
+	public End(int x, int y) {
+		setX(x);
+		setY(y);
+		setImage(new Image("file:src/p4_group_8_repo/End.png", 60, 60, true, true));
+	}
+	
+	public void setEnd() {
+		setImage(new Image("file:src/p4_group_8_repo/FrogEnd.png", 70, 70, true, true));
+		activated = true;
+	}
+	
+	public boolean isActivated() {
+		return activated;
+	}
+	
+
+}
