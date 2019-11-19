25e90b7d937e9fdb85a3ab8370632fda64d76917
psyig <psyig@exmail.nottingham.ac.uk>
2019-11-19 16:11:24 +0000

    background image to fit screen

--- src/BackgroundImage.java
+++ src/BackgroundImage.java
@@ -9,7 +9,7 @@ public class BackgroundImage extends Actor{
 	}
 	
 	public BackgroundImage(String imageLink) {
-		setImage(new Image(imageLink, 600, 800, true, true));
+		setImage(new Image(imageLink, 600, 800, false, true));
 		
 	}
 
