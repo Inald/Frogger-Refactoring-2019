3957f6647d839c24e2e3ced9af8a4a83c23bfa7f
psyig <psyig@exmail.nottingham.ac.uk>
2019-11-27 14:04:50 +0000

    testFX and stateOfTurtle added and changed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,14 +28,14 @@ public class Main extends Application {
 		getBackground().add(new Log(filePath + "/log3.png", 150, 0, 166, 0.75));
 		getBackground().add(new Log(filePath + "/log3.png", 150, 220, 166, 0.75));
 		getBackground().add(new Log(filePath + "/log3.png", 150, 440, 166, 0.75));
-		//background.add(new Log(filePath + "/log3.png", 150, 0, 166, 0.75));
+
 		getBackground().add(new Log(filePath + "/logs.png", 300, 0, 276, -2));
 		getBackground().add(new Log(filePath + "/logs.png", 300, 400, 276, -2));
-		//background.add(new Log(filePath + "/logs.png", 300, 800, 276, -2));
+
 		getBackground().add(new Log(filePath + "/log3.png", 150, 50, 329, 0.75));
 		getBackground().add(new Log(filePath + "/log3.png", 150, 270, 329, 0.75));
 		getBackground().add(new Log(filePath + "/log3.png", 150, 490, 329, 0.75));
-		//background.add(new Log(filePath + "/log3.png", 150, 570, 329, 0.75));
+
 		
 		getBackground().add(new Turtle(500, 376, -1, 130, 130));
 		getBackground().add(new Turtle(300, 376, -1, 130, 130));
@@ -43,25 +43,7 @@ public class Main extends Application {
 		getBackground().add(new WetTurtle(600, 217, -1, 130, 130));
 		getBackground().add(new WetTurtle(400, 217, -1, 130, 130));
 		getBackground().add(new WetTurtle(200, 217, -1, 130, 130));
-		//background.add(new Log(filePath + "/log2.png", 200, 100, 1));
-		//background.add(new Log(filePath + "/log2.png", 0, 100, 1));
-		//background.add(new Log(filePath + "/log2.png", 100, 120, -1));
-		//background.add(new Log(filePath + "/log2.png", 200, 120, -1));
-		//background.add(new Log(filePath + "/log2.png", 100, 140, 1));
-		//background.add(new Log(filePath + "/log2.png", 200, 140, 1));
-		//background.add(new Log(filePath + "/log2.png", 100, 160, -1));
-		//background.add(new Log(filePath + "/log2.png", 300, 160, -1));
-		//background.add(new Log(filePath + "/log2.png", 100, 180, 1));
-		//background.add(new Log(filePath + "/log2.png", 200, 180, 1));
-		//background.add(new Log(filePath + "/log2.png", 100, 200, -1));
-		//background.add(new Log(filePath + "/log2.png", 200, 200, -1));
-		//background.add(new Log(filePath + "/log2.png", 100, 220, 1));
-		//background.add(new Log(filePath + "/log2.png", 200, 220, 1));
-		//background.add(new Log(filePath + "/log2.png", 400, 220, 1));
-		//End end2 = new End();
-		//End end3 = new End();
-		//End end4 = new End();
-		//End end5 = new End();
+
 		getBackground().add(new End(13,96));
 		getBackground().add(new End(141,96));
 		getBackground().add(new End(141 + 141-13,96));
@@ -72,7 +54,7 @@ public class Main extends Application {
 		getBackground().add(new Obstacle(filePath + "/truck1"+"Right.png", 0, 649, 1, 120, 120));
 		getBackground().add(new Obstacle(filePath + "/truck1"+"Right.png", 300, 649, 1, 120, 120));
 		getBackground().add(new Obstacle(filePath + "/truck1"+"Right.png", 600, 649, 1, 120, 120));
-		//background.add(new Obstacle(filePath + "/truck1"+"Right.png", 720, 649, 1, 120, 120));
+
 		getBackground().add(new Obstacle(filePath + "/car1Left.png", 100, 597, -1, 50, 50));
 		getBackground().add(new Obstacle(filePath + "/car1Left.png", 250, 597, -1, 50, 50));
 		getBackground().add(new Obstacle(filePath + "/car1Left.png", 400, 597, -1, 50, 50));
@@ -81,9 +63,7 @@ public class Main extends Application {
 		getBackground().add(new Obstacle(filePath + "/truck2Right.png", 500, 540, 1, 200, 200));
 		getBackground().add(new Obstacle(filePath + "/car1Left.png", 500, 490, -5, 50, 50));
 		getBackground().add(new Digit(0, 30, 360, 25));
-		//background.add(obstacle);
-		//background.add(obstacle1);
-		//background.add(obstacle2);
+
 		getBackground().start();
 		primaryStage.setScene(scene);
 		primaryStage.show();
