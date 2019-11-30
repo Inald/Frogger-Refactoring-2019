1cb1be52205a8f5824d9a04f56371a44f3604d75
psyig <psyig@exmail.nottingham.ac.uk>
2019-11-30 14:51:02 +0000

    startScreen basic implementation

--- src/main/java/com/mainPack/startMenu.java
+++ src/main/java/com/mainPack/startMenu.java
@@ -1,5 +1,46 @@
 package com.mainPack;
 
-public class startMenu {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+public class startMenu extends Levels implements EventHandler<ActionEvent> {
+
+    private Stage primaryStage;
+    private Button startBtn;
+    private Button exitBtn;
+
+    public startMenu(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        startBtn = new Button();
+        startBtn.setText("Start Game");
+        startBtn.setOnAction(this);
+        exitBtn = new Button();
+        exitBtn.setText("Exit");
+        exitBtn.setOnAction(this);
+
+
+        StackPane screenLayout = new StackPane();
+        screenLayout.getChildren().add(startBtn);
+
+
+        Scene scene = new Scene(screenLayout, 600, 800);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    @Override
+    public void handle(ActionEvent event) {
+        if(event.getSource() == startBtn){
+            firstLevel newLevel = new firstLevel(this.primaryStage);
+        }
+        else if(event.getSource() == exitBtn){
+            primaryStage.close();
+        }
+    }
 }
