0c0b1a335f2bd649ebced36025104ba5420382b1
psyig <psyig@exmail.nottingham.ac.uk>
2019-12-02 13:07:12 +0000

    functions for wetTurtle made in parent class

--- src/main/java/com/mainPack/Turtle.java
+++ src/main/java/com/mainPack/Turtle.java
@@ -6,9 +6,56 @@ public class Turtle extends Actor{
 	Image turtle1;
 	Image turtle2;
 	Image turtle3;
+	Image turtle4;
 	private int speed;
 	int i = 1;
 	boolean bool = true;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public Image getTurtle1(int w, int h) {
+//		String turtle1Img;
+		Image turtle1Img;
+		turtle1Img = new Image(filePath + "/TurtleAnimation1.png", w, h, true, true);
+		return turtle1Img;
+	}
+
+	public Image getTurtle2(int w, int h) {
+		Image turtle2Img;
+		turtle2Img = new Image(filePath + "/TurtleAnimation2.png", w, h, true, true);
+		return turtle2Img;
+
+	}
+
+	public Image getTurtle3(int w, int h) {
+		Image turtle3Img;
+		turtle3Img = new Image(filePath + "/TurtleAnimation3.png", w, h, true, true);
+		return turtle3Img;
+	}
+
+	public Image wetTurtle2(int w, int h){
+		Image wetTurtle2Img;
+		wetTurtle2Img = new Image(filePath + "/TurtleAnimation2Wet.png", w, h, true, true);
+		return wetTurtle2Img;
+	}
+
+	public Image wetTurtle3(int w, int h){
+		Image wetTurtle3Img;
+		wetTurtle3Img = new Image(filePath + "/TurtleAnimation3Wet.png", w, h, true, true);
+		return wetTurtle3Img;
+	}
+
+	public Image wetTurtle4(int w, int h){
+		Image wetTurtle4Img;
+		wetTurtle4Img = new Image(filePath + "/TurtleAnimation4Wet.png", w, h, true, true);
+		return wetTurtle4Img;
+	}
+
+
+
+
 	@Override
 	public void act(long now) {
 
@@ -36,9 +83,9 @@ public class Turtle extends Actor{
 
 
 	public Turtle(int xpos, int ypos, int s, int w, int h) {
-		turtle1 = new Image(filePath + "/TurtleAnimation1.png", w, h, true, true);
-		turtle2 = new Image(filePath + "/TurtleAnimation2.png", w, h, true, true);
-		turtle3 = new Image(filePath + "/TurtleAnimation3.png", w, h, true, true);
+		turtle1 = getTurtle1(w,h);
+		turtle2 = getTurtle2(w,h);
+		turtle3 = getTurtle3(w,h);
 		setX(xpos);
 		setY(ypos);
 		speed = s;
--- src/main/java/com/mainPack/WetTurtle.java
+++ src/main/java/com/mainPack/WetTurtle.java
@@ -3,11 +3,11 @@ package com.mainPack;
 import javafx.scene.image.Image;
 
 public class WetTurtle extends Turtle{
-	Image turtle1;
-	Image turtle2;
-	Image turtle3;
+//	Image turtle1;
+//	Image turtle2;
+//	Image turtle3;
 	Image turtle4;
-	private int speed;
+	//private int speed;
 	int i = 1;
 	boolean bool = true;
 	boolean sunk = false;
@@ -17,7 +17,7 @@ public class WetTurtle extends Turtle{
 				if (now/nineHundredMillion  % 4 ==0) {
 					setImage(turtle2);
 					sunk = false;
-					
+
 				}
 				else if (now/nineHundredMillion % 4 == 1) {
 					setImage(turtle1);
@@ -26,27 +26,28 @@ public class WetTurtle extends Turtle{
 				else if (now/nineHundredMillion %4 == 2) {
 					setImage(turtle3);
 					sunk = false;
-				} else if (now/nineHundredMillion %4 == 3) {
+				} else
+				if (now/nineHundredMillion %4 == 3) {
 					setImage(turtle4);
 					sunk = true;
 				}
 			
-		move(speed , 0);
-		if (getX() > 600 && speed>0)
+		move(getSpeed() , 0);
+		if (getX() > 600 && getSpeed() > 0)
 			setX(-200);
-		if (getX() < -75 && speed<0)
+		if (getX() < -75 && getSpeed() < 0)
 			setX(600);
 	}
 	public WetTurtle(int xpos, int ypos, int s, int w, int h) {
 		super(xpos,ypos,s,w,h);
-		turtle1 = new Image(filePath + "/TurtleAnimation1.png", w, h, true, true);
-		turtle2 = new Image(filePath + "/TurtleAnimation2Wet.png", w, h, true, true);
-		turtle3 = new Image(filePath + "/TurtleAnimation3Wet.png", w, h, true, true);
-		turtle4 = new Image(filePath + "/TurtleAnimation4Wet.png", w, h, true, true);
-		setX(xpos);
-		setY(ypos);
-		speed = s;
-		setImage(turtle2);
+		turtle1 = getTurtle1(w,h);
+		turtle2 = wetTurtle2(w,h);
+		turtle3 = wetTurtle3(w,h);
+		turtle4 = wetTurtle4(w,h);
+//		setX(xpos);
+//		setY(ypos);
+//		speed = s;
+//		setImage(turtle2);
 	}
 	public boolean isSunk() {
 		return sunk;
